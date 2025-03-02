bc610d53df976b0ffccbb58bdbc3fe3ab040e4ee
JeyZ9 <ggjj4511@gmail.com>
2025-03-03 02:05:50 +0700

    config Cors

---
+++ src/main/java/com/app/manager_course/config/CorsConfig.java
@@ -0,0 +1,18 @@
+//package com.app.manager_course.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class CorsConfig implements WebMvcConfigurer {
+////    @Override
+////    public void addCorsMappings(CorsRegistry registry) {
+////        registry.addMapping("/**")
+////                .allowedOrigins("http://localhost:5173")
+////                .allowedMethods("GET", "POST", "PUT", "DELETE")
+////                .allowedHeaders("*");
+////    }
+//}
